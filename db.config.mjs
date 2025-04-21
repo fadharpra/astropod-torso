e02c046e25c62169b38991e0a902bf6f5c95beb5
Fadhli Ari Pratama <fadhli.a.pratama101@gmail.com>
2025-04-22 01:49:41 +0700

    add comma

--- db.config.mjs
+++ db.config.mjs
@@ -2,7 +2,7 @@
 export default {
   client: '@libsql/client',
   config: {
-    url: 'libsql://starpod-turso-fadharpra.aws-us-east-1.turso.io'
-    authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDUyNDMwNjQsImlkIjoiYjBhNTE3OWYtOTFjOC00YmMwLWFlMmYtN2RhOTBiMmJlNGZhIiwicmlkIjoiNDNhYjc4MmYtYzkxYS00NmQwLWI0ZGUtNzA0MGE3ZWYzMTlmIn0.Xewb5DhwIfQh81FcUEFoEzdPZ9H6uykLd-xLq-p9MBdy8FRyM2m8gVqrTZCmUk6_OA8VoCw_z4xkd0C8TdI_DQ'
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
 };
